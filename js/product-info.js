00dc2a5634a0c37817f598b0fc2edd930f889849
Rodrigo Gorga <108111681+gorga7@users.noreply.github.com>
2023-09-09 16:50:32 -0300

    Revert "Se agrego hora de comentario"
    This reverts commit d8eabdf4062b7411dc062fa6ba578367547921f9.

--- js/product-info.js
+++ js/product-info.js
@@ -128,9 +128,7 @@ contenedor.appendChild(card);
 let boton = document.getElementById("btnEnviar")
 
 boton.addEventListener("click", function(){
-    
-    let today = new Date();
-    let now = today.toLocaleString();
+
     let comentarioNuevo = document.getElementById("areaDeTexto").value;
     let cardNueva = document.createElement("div");
     cardNueva.classList.add("estilo-comentarios");
@@ -139,7 +137,7 @@ cardNueva.innerHTML=`
 <p>
 <strong>${localStorage.getItem("email")}</strong> -
 
-${now} -<br>
+${comentarioNuevo.dateTime} -<br>
 
 ${comentarioNuevo}
 </p>
