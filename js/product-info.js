999a94530707ff6eb93e8dacd459646cc14bfb28
ntabeira7 <nahueltabeira7@gmail.com>
2023-09-29 18:09:39 -0300

    Carrusel de productos.

--- js/product-info.js
+++ js/product-info.js
@@ -101,16 +101,34 @@ async function nuevoDisplay() {let products = await fetchProducts();
         </li>
         
         <li>
-            <strong>Imágenes ilustrativas</strong>
-            <br>
-            <div class="imagenes-prod">
-            <img src="${products.images[0]}">
-            <img src="${products.images[1]}">
-            <img src="${products.images[2]}">
-            <img src="${products.images[3]}">
+        <strong>Imágenes ilustrativas</strong>
+        <br>
+        <div id="carouselExample" class="carousel slide">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="${products.images[0]}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="${products.images[1]}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="${products.images[2]}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="${products.images[3]}" class="d-block w-100" alt="...">
+                </div>
             </div>
-        </li>
-        
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </li>
+    
     </ul>
     
 
